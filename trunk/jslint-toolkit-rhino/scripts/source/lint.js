eb924711b254e950585adc53d569f7b01f65ad1d
sanshi.ustc <sanshi.ustc@046c053e-80a5-11de-a3c6-f150bf15ed86>
2010-09-28 08:48:00 +0000

    Fix bugs.

--- trunk/jslint-toolkit-rhino/scripts/source/lint.js
+++ trunk/jslint-toolkit-rhino/scripts/source/lint.js
@@ -38,7 +38,9 @@ function lint(filePath, fileShortName) {
         /Extra comma\./,
         /Unnecessary semicolon\./,
         /Missing radix parameter\./,
-        /'.+' was used before it was defined\./];
+        /'.+' was used before it was defined\./,
+		/Stopping, unable to continue\. \(\d+% scanned\)\./,
+		/Too many errors\. \(\d+% scanned\)\./];
 
     var fatalErrors = [/Stopping, unable to continue\. \(\d+% scanned\)\./,
         /Too many errors\. \(\d+% scanned\)\./];
@@ -54,6 +56,10 @@ function lint(filePath, fileShortName) {
 
 
     var input = io.readFile(filePath), i, e, reason, finalError, lines;
+	// Sometimes there will be an unrecognized code in the first letter.
+	if(input.charCodeAt(0) >= 256){
+		input = input.substr(1);
+	}
     // How many lines of this file.
     lines = input.split('\n').length;
     if (lines <= 0) {
